61d7f870cd99edc94e2de8bfc3e513049b3850bc
sumitgit01 <sumitjoshi1988@gmail.com>
2026-01-14 21:03:42 +0530

    build.groovy: added def call method

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,29 +1,38 @@
-pipeline {
-    agent any
-    stages {
-        stage('build'){
-            steps{
-                script{
-                    sh """
-                        echo "building the code"
-                    """
+def call(){
+    pipeline {
+        agent any
+        stages {
+            stage('build'){
+                steps{
+                    script{
+                        sh """
+                            echo "building the code"
+                        """
+                    }
                 }
             }
-        }
-        stage('static code analysis'){
-            steps{
-                script{
-                    println "static code analysis using Sonar"
+            stage('static code analysis'){
+                steps{
+                    script{
+                        println "static code analysis using Sonar"
+                    }
                 }
             }
-        }
-        stage('build docker image and push to artifactory'){
-            steps{
-                script{
-                    println "build docker image and push"
+            stage('build docker image and push to artifactory'){
+                steps{
+                    script{
+                        println "build docker image and push"
+                    }
                 }
             }
+            stage('push helm packages to artifactory'){
+                steps{
+                    script{
+                        println "push Helm packages to artifactory"
+                    }
+                }
+            }
+            
         }
-        
     }
 }
