f9411b2f097ec1e09d5de6a5ae9a783dfb116d2d
sumitgit01 <sumitjoshi1988@gmail.com>
2026-01-17 17:06:36 +0530

    Build.groovy: updated build steps

--- vars/build.groovy
+++ vars/build.groovy
@@ -6,14 +6,8 @@ def call(){
                 steps{
                     script{
                         sh """
-                            echo "building the code"
-                            java -version
-                            mvn -version
-                            export JAVA_HOME=/datadisk/soft/jdk-21.0.9
-                            export PATH=\\$JAVA_HOME/bin:\\$PATH
-                            java -version
-                            mvn -version
-                            mvn clean install
+                            chmod +x build.sh
+                            ./build.sh
                         """
                     }
                 }
